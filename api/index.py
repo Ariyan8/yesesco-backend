a887c72ff60cb21a12c71e6b9dec61b8a39988e5
Ariyan SADRAEI <sadraei.ariyan@yahoo.com>
2026-08-30 18:20:57 +0330

    Fix routing to enable swagger docs

--- api/index.py
+++ api/index.py
@@ -1,17 +1,7 @@
 import sys
 import os
 
-# اضافه کردن مسیر ریشه پروژه به sys.path جهت لود شدن ماژول app
+# اضافه کردن مسیر ریشه پروژه به sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app.main import app
-
-# تعریف پاسخ برای تمام آدرس‌های ریشه احتمالی که ورسل فوروارد می‌کند
-@app.get("/")
-@app.get("/api")
-@app.get("/api/index.py")
-def read_root():
-    return {
-        "status": "online",
-        "message": "YesESCo API is running successfully on Vercel"
-    }
